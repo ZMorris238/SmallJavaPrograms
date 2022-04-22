b3e39b22a61963b3e0730fc8ceaaac7ae118465f
Zack Morris <zjmorris97@gmail.com>
2022-04-21 21:19:52 -0500

    edited HelloWorld program

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String str = "Hello World!";
+
+        System.out.println(str);
     }
 }
